f181e5e5ffdd3cad4963eeaa2840a1c5b7c0b453
RamuMaruri <114585719+RamuMaruri@users.noreply.github.com>
2023-05-19 08:23:15 +0530

    Update code.js

--- code.js
+++ code.js
@@ -3,19 +3,13 @@ let beats, time, median, sum = 0, length = 0, maxiDate;
 
 for (let i = 472; i <= array.length; i++) {
     const element = array[i];
-
     beats = array.map((item) => item.beatsPerMinute)
-
     beats.forEach(num => {
         sum += num;
     })
-
-
     length = beats.length;
     median = sum / length;
-
     time = array.map((item) => item.timestamps)
-
 }
 maxiDate = new Date(
     Math.max(
@@ -28,6 +22,4 @@ console.log("MAX Beats: " + Math.max(...beats));
 console.log("MIN Beats: " + Math.min(...beats));
 console.log("MEDIAN Beats: " + median);
 console.log("latestDataTimestamp: " +maxiDate);
-
-
 
